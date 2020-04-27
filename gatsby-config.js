b26173479000dd6911a62d4b2dc9d6a811803ac2
Kevin K <kevinkim75@gmail.com>
2020-04-27 11:35:15 -0400

    Changed site titlanged site title and metadata.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Freelance Web/App Developer`,
+    description: `I build websites and apps that are better looking, more enjoyable, and more useful to people.`,
     author: `@gatsbyjs`,
   },
   plugins: [
